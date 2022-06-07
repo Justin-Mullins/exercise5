4eabc0851d03ee3dc612dcd3fda2fb8e0e8f36ac
Justin-Mullins <justin.mullins009@gmail.com>
2022-06-06 23:04:58 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+
+Exercise 5
+
+Pig Latin (http://mng.bz/YrON) is a common childrens “secret” language in English-
+speaking countries. (Its normally secret among children who forget that their parents
+were once children themselves.) The rules for translating words from English into Pig
+Latin are quite simple:
+ If the word begins with a vowel (a, e, i, o, or u), add “way” to the end of the
+word. So “air” becomes “airway” and “eat” becomes “eatway.”
+ If the word begins with any other letter, then we take the first letter, put it on
+the end of the word, and then add “ay.” Thus, “python” becomes “ythonpay”
+and “computer” becomes “omputercay.”
+(And yes, I recognize that the rules can be made more sophisticated. Lets keep it sim-
+ple for the purposes of this exercise.)
+For this exercise, write a Python function ( pig_latin ) that takes a string as input,
+assumed to be an English word. The function should return the translation of this word
+into Pig Latin. You may assume that the word contains no capital letters or punctuation.
+This exercise is not meant to help you translate documents into Pig Latin for your
+job. (If that is your job, then I really have to question your career choices.) However, it
+demonstrates some of the powerful techniques that you should know when working
+with sequences, including searches, iteration, and slices. Its hard to imagine a Python
+program that does not include any of these techniques.
+
+'''
+
+def pig_latin(string1):
+    first_letter = string1[0]
+    if first_letter in ['a', 'e', 'i', 'o', 'u']:
+        string1 += 'way'
+    else:
+        string1 = string1[1:] + first_letter + 'ay'
+    return string1
+ 
+print(pig_latin('computer')) 
+print(pig_latin('python'))
+print(pig_latin('arrow'))
